ed06d44c8dca7ea743bd44a734020d1cf41cfe9a
kammous <kimo_ammous@yahoo.fr>
2020-11-23 23:38:51 +0100

    add entities

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,10 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'person',
+        loadChildren: () => import('./ws/person/person.module').then(m => m.WsPersonModule),
+      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
